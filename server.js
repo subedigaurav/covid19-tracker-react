aeac7f4e288c1739203b7cacafee1e6bb88f0454
Gaurav Subedi <subedi.gasu15@gmail.com>
2020-04-20 19:16:20 +0545

    cleaned the scripts

--- server.js
+++ server.js
@@ -3,14 +3,10 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon(__dirname + '/public/favicon.ico'));
-
+app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/ping', function (req, res) {
-  return res.send('pong');
-});
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
